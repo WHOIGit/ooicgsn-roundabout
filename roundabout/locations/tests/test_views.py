82f7c9f8a11220ad85b223275262196fe9d640d1
Ethan Andrews <eandrews@whoi.edu>
2019-03-06 16:11:09 -0500

    Add Location views permissions tests

---
+++ roundabout/locations/tests/test_views.py
@@ -0,0 +1,45 @@
+from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+from django.contrib.auth.models import User, Permission, Group
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+
+from roundabout.locations import views
+from roundabout.locations.models import Location
+
+
+class LocationDetailTests(TestCase):
+
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'testpass'
+        User = get_user_model()
+        content_type = ContentType.objects.get_for_model(Location)
+        self.permission = Permission.objects.get(content_type=content_type, codename='add_location')
+        group_name = "admin"
+        self.group = Group(name=group_name)
+        self.group.save()
+        self.user = User.objects.create_user(self.username, password=self.password, is_active=True)
+
+    def tearDown(self):
+        self.user.delete()
+        self.group.delete()
+
+    def test_home_view_wrong_user_access(self):
+        """user with NO location permission should not have access
+        """
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse('locations:locations_home'))
+        assert response.status_code == 302
+
+    def test_home_view_can_access(self):
+        """user in location permission should have access
+        """
+        self.user.groups.add(self.group)
+        self.user.user_permissions.add(self.permission)
+        self.user.save()
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse('locations:locations_home'))
+        assert response.status_code == 200
