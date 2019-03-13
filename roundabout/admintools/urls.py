93bb6cfdffdda830768adc336e20a333f7808691
Ethan Andrews <eandrews@whoi.edu>
2019-03-13 14:29:55 -0400

    Add "Create Printer" functionality for admin users

--- roundabout/admintools/urls.py
+++ roundabout/admintools/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'admintools'
 urlpatterns = [
     path('printers/', view=views.PrinterListView.as_view(), name='printers_home'),
+    path('printers/add/', view=views.PrinterCreateView.as_view(), name='printers_add'),
 ]
--- roundabout/admintools/views.py
+++ roundabout/admintools/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import View, DetailView, ListView, RedirectView, UpdateView, CreateView, DeleteView, TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+from .forms import PrinterForm
 from .models import Printer
 
 # Create your views here.
@@ -10,3 +12,14 @@ class PrinterListView(LoginRequiredMixin, ListView):
     model = Printer
     template_name = 'printer_list.html'
     context_object_name = 'printers'
+
+
+class PrinterCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Printer
+    form_class = PrinterForm
+    context_object_name = 'printer'
+    permission_required = 'admintools.add_printer'
+    redirect_field_name = 'home'
+
+    def get_success_url(self):
+        return reverse('admintools:printers_home', )
