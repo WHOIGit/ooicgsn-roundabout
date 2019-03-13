8f24ffc65e60753d691148a181e97708c43124cc
Ethan Andrews <eandrews@whoi.edu>
2019-03-13 14:48:19 -0400

    Add "Update" and "Delete" functionality for Printers

--- roundabout/admintools/urls.py
+++ roundabout/admintools/urls.py
@@ -6,4 +6,6 @@ app_name = 'admintools'
 urlpatterns = [
     path('printers/', view=views.PrinterListView.as_view(), name='printers_home'),
     path('printers/add/', view=views.PrinterCreateView.as_view(), name='printers_add'),
+    path('printers/edit/<int:pk>/', view=views.PrinterUpdateView.as_view(), name='printers_update'),
+    path('printers/delete/<int:pk>/', view=views.PrinterDeleteView.as_view(), name='printers_delete'),
 ]
--- roundabout/admintools/views.py
+++ roundabout/admintools/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import View, DetailView, ListView, RedirectView, UpdateView, CreateView, DeleteView, TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from .forms import PrinterForm
 from .models import Printer
 
-# Create your views here.
+# Printer functionality
 
 class PrinterListView(LoginRequiredMixin, ListView):
     model = Printer
@@ -23,3 +23,20 @@ class PrinterCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView)
 
     def get_success_url(self):
         return reverse('admintools:printers_home', )
+
+class PrinterUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Printer
+    form_class = PrinterForm
+    context_object_name = 'printer'
+    permission_required = 'admintools.add_printer'
+    redirect_field_name = 'home'
+
+    def get_success_url(self):
+        return reverse('admintools:printers_home', )
+
+
+class PrinterDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Printer
+    success_url = reverse_lazy('admintools:printers_home')
+    permission_required = 'admintools.delete_printer'
+    redirect_field_name = 'home'
