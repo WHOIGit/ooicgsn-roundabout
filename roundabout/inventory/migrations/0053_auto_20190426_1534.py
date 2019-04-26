3b9fe5c9c2feb9f2539aeb488849b05fcfd79732
Ethan Andrews <eandrews@whoi.edu>
2019-04-26 12:02:29 -0400

    Add comment

--- roundabout/inventory/migrations/0053_auto_20190426_1534.py
+++ roundabout/inventory/migrations/0053_auto_20190426_1534.py
@@ -8,11 +8,12 @@ def port_inventory_to_revisions(apps, schema_editor):
 
     Inventory = apps.get_model('inventory', 'Inventory')
 
+    # Set initial Revision for existing Inventory items
     for inv in Inventory.objects.all():
         if inv.part:
             inv.revision = inv.part.revisions.all().first()
         else:
-            inv.revision = None    
+            inv.revision = None
         inv.save()
 
 
