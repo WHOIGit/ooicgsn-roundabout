fb2b87e144685d17c852a7af959491fe8bfe8608
Ethan Andrews <eandrews@whoi.edu>
2019-05-16 13:24:30 -0400

    Add ability to lock field to default value for all items to UDF

--- roundabout/userdefinedfields/forms.py
+++ roundabout/userdefinedfields/forms.py
@@ -7,7 +7,9 @@ class UserDefinedFieldForm(forms.ModelForm):
 
     class Meta:
         model = Field
-        fields = ['field_name', 'field_description', 'field_type', 'field_default_value', 'global_for_part_types' ]
+        fields = ['field_name', 'field_description', 'field_type', 'field_default_value',
+                  'value_is_locked', 'global_for_part_types' ]
         labels = {
-
+            'value_is_locked': 'Lock this field value for all Items',
+            'global_for_part_types': 'Global field for all Parts of this Type',
         }
