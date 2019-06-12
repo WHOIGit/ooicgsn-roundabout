e691ac87b0dcd08ad27a2d01c7284585125ed3ba
Ethan Andrews <eandrews@whoi.edu>
2019-06-12 10:47:35 -0400

    Limit custom OBS Location Type options to only OBS site

--- roundabout/locations/forms.py
+++ roundabout/locations/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.forms.models import inlineformset_factory
+from django.contrib.sites.models import Site
 
 from .models import Location
+
 
 
 class LocationForm(forms.ModelForm):
@@ -12,3 +14,26 @@ class LocationForm(forms.ModelForm):
         labels = {
         'location_id': 'Location ID'
     }
+
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+
+        # Default Location Types
+        LOC_TYPES = (
+            ('', ''),
+            ('Array', 'Array'),
+            ('Mooring', 'Mooring'),
+        )
+
+        # Custom Location Types for OBS
+        LOC_TYPES_OBS = (
+            ('', ''),
+            ('Instrument', 'Instrument'),
+        )
+
+        current_site = Site.objects.get_current()
+
+        if current_site.domain == 'obs-rdb.whoi.edu':
+            self.fields['location_type'].choices = LOC_TYPES_OBS
+        else:
+            self.fields['location_type'].choices = LOC_TYPES
