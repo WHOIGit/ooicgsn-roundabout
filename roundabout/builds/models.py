c7a77ebb9f49620e01fb16b0b24decf007deb41e
Ethan Andrews <eandrews@whoi.edu>
2019-06-26 10:43:38 -0400

    Add BuildAction model

--- roundabout/builds/models.py
+++ roundabout/builds/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 from roundabout.locations.models import Location
 from roundabout.assemblies.models import Assembly
+from roundabout.users.models import User
 
 # Build model
 
@@ -20,3 +21,51 @@ class Build(models.Model):
 
     def __str__(self):
         return '%s - %s' % (self.build_number, self.assembly.name)
+
+
+class BuildAction(models.Model):
+    BUILDADD = 'buildadd'
+    LOCATIONCHANGE = 'locationchange'
+    SUBASSEMBLYCHANGE = 'subassemblychange'
+    ASSIGNTODEPLOYMENT = 'assigntodeployment'
+    REMOVEFROMDEPLOYMENT = 'removefromdeployment'
+    DEPLOYMENTBURNIN = 'deploymentburnin'
+    DEPLOYMENTTOSEA = 'deploymenttosea'
+    DEPLOYMENTRECOVER = 'deploymentrecover'
+    TEST = 'test'
+    NOTE = 'note'
+    HISTORYNOTE = 'historynote'
+    TICKET = 'ticket'
+    FLAG = 'flag'
+    MOVETOTRASH = 'movetotrash'
+    ACT_TYPES = (
+        (BUILDADD, 'Add Build'),
+        (LOCATIONCHANGE, 'Location Change'),
+        (SUBASSEMBLYCHANGE, 'Subassembly Change'),
+        (ASSIGNTODEPLOYMENT, 'Assign to Deployment'),
+        (REMOVEFROMDEPLOYMENT, 'Remove from Deployment'),
+        (DEPLOYMENTBURNIN, 'Deployment Burnin'),
+        (DEPLOYMENTTOSEA, 'Deployment to Sea'),
+        (DEPLOYMENTRECOVER, 'Deployment Recovered'),
+        (TEST, 'Test'),
+        (NOTE, 'Note'),
+        (HISTORYNOTE, 'Historical Note'),
+        (TICKET, 'Work Ticket'),
+        (FLAG, 'Flag'),
+        (MOVETOTRASH, 'Move to Trash'),
+    )
+    action_type = models.CharField(max_length=20, choices=ACT_TYPES)
+    created_at = models.DateTimeField(default=timezone.now)
+    detail = models.TextField(blank=True)
+    user = models.ForeignKey(User, related_name='actions',
+                             on_delete=models.SET_NULL, null=True, blank=False)
+    location = TreeForeignKey(Location, related_name='actions',
+                              on_delete=models.SET_NULL, null=True, blank=False)
+    build = models.ForeignKey(Build, related_name='actions',
+                                 on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at', 'action_type']
+
+    def __str__(self):
+        return self.get_action_type_display()
