00ca578da6e071657bc20737dac178ac3071d1d2
Ethan Andrews <eandrews@whoi.edu>
2019-10-07 16:08:23 -0400

    Add nested Part field to Inventory API

--- roundabout/inventory/api/serializers.py
+++ roundabout/inventory/api/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 
 from ..models import Inventory
 from roundabout.locations.api.serializers import LocationSerializer
+from roundabout.parts.api.serializers import PartSerializer
 
 
 class InventorySerializer(serializers.ModelSerializer):
     location = LocationSerializer(read_only=True)
+    part = PartSerializer(read_only=True)
 
     class Meta:
         model = Inventory
