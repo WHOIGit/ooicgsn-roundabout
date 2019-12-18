c5d2ad918817441344b434946aea818dfae00c23
Ethan Andrews <eandrews@whoi.edu>
2019-12-18 14:20:02 -0500

    Add green badge/bg color for template mode display

--- roundabout/context_processors.py
+++ roundabout/context_processors.py
@@ -1,9 +1,18 @@
 # make thes App, Namespace, URL names available for templates
 def template_resolver_names(request):
+    # variable to identify if app is a "Template" app or "Real Thing" app
+    template_app_list =['parts', 'assemblies', 'locations']
+
+    if request.resolver_match.app_name in template_app_list:
+        template_app_mode = True
+    else:
+        template_app_mode = False
+
     return {
         'app_name': request.resolver_match.app_name,
         'namespace': request.resolver_match.namespace,
-        'url_name': request.resolver_match.url_name
+        'url_name': request.resolver_match.url_name,
+        'template_app_mode': template_app_mode,
     }
 
 # Set sitewide template variable for headings/labels display
