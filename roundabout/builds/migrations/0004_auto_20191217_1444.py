e5320624920d357d6399f1e9cd964c1b3694c17e
Ethan Andrews <eandrews@whoi.edu>
2020-01-10 09:32:45 -0500

    Update Build migration file to remove Unique constraint

--- roundabout/builds/migrations/0004_auto_20191217_1444.py
+++ roundabout/builds/migrations/0004_auto_20191217_1444.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='build',
             name='build_number',
-            field=models.CharField(db_index=True, max_length=255, unique=True),
+            field=models.CharField(db_index=True, max_length=255, unique=False),
         ),
     ]
