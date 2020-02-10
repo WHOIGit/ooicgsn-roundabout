fee670b50a4347d4bd70153611ebafb99fe57739
Stephanie Petillo <spetillo@whoi.edu>
2020-02-10 14:42:54 -0500

    Changed license header commenting to use #. Cleaned up script to update license headers. Script does not update Bash script headers.

--- scripts/update_copyright_headers.sh
+++ scripts/update_copyright_headers.sh
@@ -2,58 +2,55 @@
 
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 code_dir="${scripts_dir}/.."
+header="${code_dir}/docs/copyright/gpl2_and_later_header.txt"
 
-#declare -a file_type_list = ("py_yml" "js_css" "html")
-
-for file_type in "py_yml" "js_css" "html"
-#for file_type in "${file_type_list[@]}"
-do
-    echo "$file_type"
-    
+for file_type in "py_yml" "js_css_h_c_cpp" "html" 
+do    
     # all .py, .yml, .js., .css, and .html files
     case $file_type in
         
         py_yml)
-            comment_start = '"""'
-            comment_end = '"""'
-            regex_match = 'py\|yml\'
+            comment_start='"""'
+            comment_end='"""'
+            regex_match='.*/.*\.\(py\|yml\)'
             ;;
-        js_css)
-            comment_start = '/*'
-            comment_end = '*/'
-            regex_match = 'js\|css\'
+        
+        js_css_h_c_cpp)
+            comment_start='/*'
+            comment_end='*/'
+            regex_match='.*/.*\.\(js\|css\|h\|c\|cpp\)'
             ;;
+        
         html)
-            comment_start = '<!--'
-            comment_end = '-->'
-            regex_match = 'html\'
-            ;;
-        *) # default to C/C++ commenting (already in header file)
-            comment_start = ''
-            comment_end = ''
-            regex_match = 'h\|cpp\|c\'
+            comment_start='<!--'
+            comment_end='-->'
+            regex_match='.*/.*\.\(html\)'
             ;;
     esac
     
-    find $code_dir -regex '.*/.*\.\($regex_match)' | while read -r file; do    
+    find $code_dir -regex $regex_match | while read -r file; do    
 
-        if [ "$file" != "__init__.py" ];
+        if [[ "${file}" != *"__init__.py"* ]]; then # Ignore auto-generated __init__.py
+            
+            echo -e "$comment_start\n$(cat $header)" > /tmp/copyright_header_tmp
+            
+            endtext='If not, see <http:\/\/www.gnu.org\/licenses\/>.'
+            l=$(grep -n "$endtext" $file | tail -1 | cut -d ":" -f 1)            
+            if [[ $l == "" ]]; then
+                l=0
+                echo -e "$comment_end\n" >> /tmp/copyright_header_tmp
+            else
+                l=$(($l+1))
+            fi
+            tail -n +$l $file | sed '/./,$!d' > /tmp/copyright_file_tmp
+            
+            cat /tmp/copyright_header_tmp /tmp/copyright_file_tmp > $file
 
-           endtext='If not, see <http:\/\/www.gnu.org\/licenses\/>.'
-           l=$(grep -n "$endtext" $file | tail -1 | cut -d ":" -f 1)    
-           if [[ $l == "" ]]; then
-               l=0
-           else
-               l=$(($l+1))
-           fi
-           tail -n +$l $file | sed '/./,$!d' > /tmp/copyright_tmp
-
-           cat $comment_start $code_dir/docs/copyright/gpl2_and_later_header.txt $comment_end /tmp/copyright_tmp > $file
-           
-           rm /tmp/copyright_tmp 
-           
-           echo "Updated" `realpath $file`
-           
+            rm /tmp/copyright_header_tmp
+            rm /tmp/copyright_file_tmp 
+            
+            echo "Updated" `realpath $file`
+            
         fi
     done
 done
