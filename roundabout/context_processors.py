f19a3b7873879c5b81c61425fd0f8dad93465ee0
Ethan Andrews <eandrews@whoi.edu>
2020-02-13 11:03:07 -0500

    Fix environ get string, label display now works

--- roundabout/context_processors.py
+++ roundabout/context_processors.py
@@ -28,25 +28,28 @@ env = environ.Env()
 
 def template_set_app_labels(request):
     # Set pattern variables from .env configuration
-    RDB_LABEL_ASSEMBLIES_SINGULAR = env.bool('RDB_LABEL_ASSEMBLIES_SINGULAR', default='Assembly')
-    RDB_LABEL_ASSEMBLIES_PLURAL = env.bool('RDB_LABEL_ASSEMBLIES_PLURAL', default='Assemblies')
-    RDB_LABEL_BUILDS_SINGULAR = env.bool('RDB_LABEL_BUILDS_SINGULAR', default='Build')
-    RDB_LABEL_BUILDS_PLURAL = env.bool('RDB_LABEL_BUILDS_PLURAL', default='Builds')
-    RDB_LABEL_DEPLOYMENTS_SINGULAR = env.bool('RDB_LABEL_DEPLOYMENTS_SINGULAR', default='Deployment')
-    RDB_LABEL_DEPLOYMENTS_PLURAL = env.bool('RDB_LABEL_DEPLOYMENTS_PLURAL', default='Deployments')
-    RDB_LABEL_INVENTORY_SINGULAR = env.bool('RDB_LABEL_INVENTORY_SINGULAR', default='Inventory')
-    RDB_LABEL_INVENTORY_PLURAL = env.bool('RDB_LABEL_INVENTORY_PLURAL', default='Inventory')
+    RDB_LABEL_ASSEMBLIES_SINGULAR = env('RDB_LABEL_ASSEMBLIES_SINGULAR', default='Assembly')
+    RDB_LABEL_ASSEMBLIES_PLURAL = env('RDB_LABEL_ASSEMBLIES_PLURAL', default='Assemblies')
+    RDB_LABEL_BUILDS_SINGULAR = env('RDB_LABEL_BUILDS_SINGULAR', default='Build')
+    RDB_LABEL_BUILDS_PLURAL = env('RDB_LABEL_BUILDS_PLURAL', default='Builds')
+    RDB_LABEL_DEPLOYMENTS_SINGULAR = env('RDB_LABEL_DEPLOYMENTS_SINGULAR', default='Deployment')
+    RDB_LABEL_DEPLOYMENTS_PLURAL = env('RDB_LABEL_DEPLOYMENTS_PLURAL', default='Deployments')
+    RDB_LABEL_INVENTORY_SINGULAR = env('RDB_LABEL_INVENTORY_SINGULAR', default='Inventory')
+    RDB_LABEL_INVENTORY_PLURAL = env('RDB_LABEL_INVENTORY_PLURAL', default='Inventory')
 
     labels = {
         # Assemblies
         'label_assemblies_app_singular': RDB_LABEL_ASSEMBLIES_SINGULAR,
         'label_assemblies_app_plural': RDB_LABEL_ASSEMBLIES_PLURAL,
-        # Assemblies
+        # Builds
         'label_builds_app_singular': RDB_LABEL_BUILDS_SINGULAR,
         'label_builds_app_plural': RDB_LABEL_BUILDS_PLURAL,
         # Deployments
         'label_deployments_app_singular': RDB_LABEL_DEPLOYMENTS_SINGULAR,
         'label_deployments_app_plural': RDB_LABEL_DEPLOYMENTS_PLURAL,
+        # Deployments
+        'label_inventory_app_singular': RDB_LABEL_INVENTORY_SINGULAR,
+        'label_inventory_app_plural': RDB_LABEL_INVENTORY_PLURAL,
     }
 
     return labels
