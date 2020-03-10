7c1e7e15a99906d5de728b15484b656d3d5e68e2
Ethan Andrews <eandrews@whoi.edu>
2020-03-10 15:54:56 -0400

    Update navtrees for Build/Inventory

--- roundabout/builds/views.py
+++ roundabout/builds/views.py
@@ -52,11 +52,12 @@ def load_builds_navtree(request):
         return render(request, 'builds/ajax_build_navtree.html', {'locations': locations})
     else:
         build_pk = node_id.split('_')[1]
-        build = Build.objects.prefetch_related('assembly__assembly_parts').prefetch_related('inventory').get(id=build_pk)
-        return render(request, 'builds/build_tree_assembly.html', {'assembly_parts': build.assembly.assembly_parts,
+        build = Build.objects.prefetch_related('assembly_revision__assembly_parts').prefetch_related('inventory').get(id=build_pk)
+        return render(request, 'builds/build_tree_assembly.html', {'assembly_parts': build.assembly_revision.assembly_parts,
                                                                    'inventory_qs': build.inventory,
                                                                    'location_pk': build.location_id,
                                                                    'build_pk': build_pk, })
+
 
 # Internal function to copy Inventory items for Build Snapshots
 def _make_tree_copy(root_part, new_location, build_snapshot, parent=None ):
