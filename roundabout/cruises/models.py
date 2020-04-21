94d44492b9d3d921c7676dbd642857a68df14d46
Ethan Andrews <eandrews@whoi.edu>
2020-04-21 14:29:33 -0400

    Initial Vessel model

--- roundabout/cruises/models.py
+++ roundabout/cruises/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Cruises app models
+
+class Vessel(models.Model):
+    BOOLEAN_CHOICES = (
+        (True, 'Yes'), (False, 'No')
+    )
+    vessel_name = models.CharField(max_length=100)
+    ICES_code = models.CharField(max_length=10, null=False, blank=True)
+    operator = models.CharField(max_length=100, null=False, blank=True)
+    call_sign = models.CharField(max_length=10, null=False, blank=True)
+    MMSI_number = models.IntegerField(null=True)
+    IMO_number = models.IntegerField(null=True)
+    length = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    max_speed = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    max_draft = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    designation = models.CharField(max_length=10, null=False, blank=True)
+    active = models.BooleanField(choices=BOOLEAN_CHOICES, null=True, default=True)
+    R2R = models.BooleanField(choices=BOOLEAN_CHOICES, null=True, default=True)
+
+    class Meta:
+        ordering = ('vessel_name',)
+
+    def __str__(self):
+        return self.vessel_name
