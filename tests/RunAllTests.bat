03d4edfea1ec677c7806ff2004a91b1823ff12d2
joannekoch <65416034+joannekoch@users.noreply.github.com>
2020-06-30 09:39:54 -0400

    Create RunAllTests.bat
    Runs all Roundabout Selenium Webdriver automated tests in windows cmd prompt.

---
+++ tests/RunAllTests.bat
@@ -0,0 +1,2 @@
+rem Runs all Roundabout Selenium Webdriver automated tests in windows cmd prompt. Takes about 11 minutes to run. 
+start cmd.exe /k "node AddEditLocations.js > RoundAboutTesting.log & node AddEditParts.js >> RoundAboutTesting.log & node AddEditAssemblies.js >> RoundAboutTesting.log & node AddEditInventory.js >> RoundAboutTesting.log & node AddBuilds.js >> RoundAboutTesting.log & node RetireBuilds.js >> RoundAboutTesting.log & node DeleteAssemblies.js >> RoundAboutTesting.log & node DeleteParts.js >> RoundAboutTesting.log & node DeleteLocations.js >> RoundAboutTesting.log"
