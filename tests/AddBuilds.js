a9efe4d065f7f99c69b6023f6d60400dc4b84dc1
joannekoch <65416034+joannekoch@users.noreply.github.com>
2020-07-02 10:24:34 -0400

    Add Firefox

--- tests/AddBuilds.js
+++ tests/AddBuilds.js
@@ -6,14 +6,19 @@ console.log('Running Add Builds Test');
 const { Builder, By, Key, until, a, WebElement, promise } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
-const chromedriver = require('chromedriver');
-const { exception } = require('console');
 
 var driver;
+var myArgs = process.argv.slice(2);
 
 (async function addBuilds() {
 
-    driver = new Builder().forBrowser('chrome').build();
+    // First argument specifies the Browser type, chrome is default if no argument is supplied
+    if ((myArgs[0] == 'chrome') || (myArgs.length == 0)) {        
+        driver = new Builder().forBrowser('chrome').build();
+    }
+    else {
+        driver = new Builder().forBrowser('firefox').build();
+    }
 
     // Step # | name | target | value
     // 1 | open | https://ooi-cgrdb-staging.whoi.net/ | 
@@ -36,12 +41,16 @@ var driver;
         // Add build with non null assembly template, build number, and location
         await driver.findElement(By.linkText("Builds")).click();
         // 4 | click | linkText=Create New Build | 
+        await driver.wait(until.elementLocated(By.linkText("Create New Build")));
         await driver.findElement(By.linkText("Create New Build")).click();
+        await new Promise(r => setTimeout(r, 2000));  //needed for firefox build number to populate
         // 5 | select | id=id_assembly | label=Test Glider 1
         {
             const dropdown = await driver.findElement(By.id("id_assembly"));
             await dropdown.findElement(By.xpath("//option[. = 'Test Glider 1']")).click();
         }
+        await new Promise(r => setTimeout(r, 2000));  //needed for firefox build number to populate
+
         // 6 | select | id=id_location | label=--- Test Child
         {
             const dropdown = await driver.findElement(By.id("id_location"));
@@ -53,7 +62,32 @@ var driver;
         await driver.findElement(By.css(".controls > .btn")).click();
         // 9 | click | linkText=Create New Build | 
 
+        // Verify Build is created in Test Child Location
+        await driver.findElement(By.css(".btn-outline-primary:nth-child(1)")).click(); // search button
+        // 20 | click | id=field-select_c_r0 | 
+        await driver.wait(until.elementLocated(By.id("field-select_c_r0")));
+        await driver.findElement(By.id("field-select_c_r0")).click();
+        // 21 | select | id=field-select_c_r0 | label=Location
+        {
+            const dropdown = await driver.findElement(By.id("field-select_c_r0"));
+            await dropdown.findElement(By.xpath("//option[. = 'Location']")).click();
+        }
+        // 22 | select | id=qfield-lookup_c_r0 | label=Exact
+        {
+            const dropdown = await driver.findElement(By.id("qfield-lookup_c_r0"));
+            await dropdown.findElement(By.xpath("//option[. = 'Exact']")).click();
+        }
+        // 23 | type | id=field-query_c_r0 | Lost
+        await driver.findElement(By.id("field-query_c_r0")).sendKeys("Test Child");
+        // 24 | click | id=searchform-submit-button | 
+        await driver.findElement(By.id("searchform-submit-button")).click();
+        // 25 | click | css=.even a | 
+        await new Promise(r => setTimeout(r, 2000));
+        await driver.findElement(By.xpath("//p[contains(.,'1 items match your search!')]"));
+
         // Add build with null assembly template, assembly revision, build number or location
+        await driver.findElement(By.linkText("Builds")).click();
+        await driver.wait(until.elementLocated(By.linkText("Create New Build")));
         await driver.findElement(By.linkText("Create New Build")).click();
         // 10 | select | id=id_assembly | label=Test Glider 1
         {
