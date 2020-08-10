498b8e9fcc44c1d8b716de7486bd55f7eec61605
Joanne Koch <jkoch@whoi.edu>
2020-08-10 09:27:55 -0400

    Circleci debugging

--- tests/RunAllTests-Chrome-Linux.bat
+++ tests/RunAllTests-Chrome-Linux.bat
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Runs all Roundabout Selenium Webdriver automated tests in linux Docker container. Takes about 11 minutes to run.
-node AddEditLocations.js chrome headless > RoundAboutTesting.log && node AddEditParts.js chrome headless >> RoundAboutTesting.log && node AddEditAssemblies.js chrome headless >> RoundAboutTesting.log && node AddEditInventory.js chrome headless >> RoundAboutTesting.log && node AddBuilds.js chrome headless >> RoundAboutTesting.log && node RetireBuilds.js chrome headless >> RoundAboutTesting.log && node DeleteAssemblies.js chrome headless >> RoundAboutTesting.log && node DeleteParts.js chrome headless >> RoundAboutTesting.log && node DeleteLocations.js chrome headless >> RoundAboutTesting.log
+node AddEditLocations.js chrome headless && node AddEditParts.js chrome headless  && node AddEditAssemblies.js chrome headless  && node AddEditInventory.js chrome headless  && node AddBuilds.js chrome headless  && node RetireBuilds.js chrome headless  && node DeleteAssemblies.js chrome headless  && node DeleteParts.js chrome headless  && node DeleteLocations.js chrome headless 
