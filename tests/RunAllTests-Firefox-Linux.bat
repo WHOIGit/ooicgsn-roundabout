4b0551451e220630cc91c8d493e3a8fe5e8742b2
Joanne Koch <jkoch@whoi.edu>
2020-08-26 12:57:30 -0400

    Change echo Run to Running

--- tests/RunAllTests-Firefox-Linux.bat
+++ tests/RunAllTests-Firefox-Linux.bat
@@ -3,7 +3,7 @@
 set -e
 
 # Runs all Roundabout Selenium Webdriver automated tests in linux Docker container. Takes about 11 minutes to run.
-echo Run Firefox Tests
+echo Running Firefox Tests
 
 val=$(node AddEditLocations.js firefox headless)
 echo $val
