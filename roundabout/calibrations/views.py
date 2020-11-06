957903f4ef24b3c5dd964308687fbbf04eebb2ec
Mario Carloni <mcarloni4@gmail.com>
2020-11-06 13:46:29 -0500

    fixed deploy to field render, auto-delete nameless CCC Events, fix migration conflict

--- roundabout/calibrations/views.py
+++ roundabout/calibrations/views.py
@@ -37,7 +37,7 @@ from sigfig import round
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.forms.models import inlineformset_factory, BaseInlineFormSet
-from .utils import handle_reviewers
+from .utils import handle_reviewers, check_events
 
 # Handles creation of Calibration Events, Names,and Coefficients
 class EventValueSetAdd(LoginRequiredMixin, AjaxFormMixin, CreateView):
@@ -484,6 +484,7 @@ class EventCoeffNameUpdate(LoginRequiredMixin, PermissionRequiredMixin, AjaxForm
         part_calname_form.save()
         part_cal_copy_form.save()
         _create_action_history(self.object, Action.UPDATE, self.request.user)
+        check_events()
         response = HttpResponseRedirect(self.get_success_url())
         if self.request.is_ajax():
             data = {
@@ -550,6 +551,7 @@ class EventCoeffNameDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteVi
             'object_type': self.object.get_object_type(),
         }
         self.object.delete()
+        check_events()
         return JsonResponse(data)
 
     def get_success_url(self):
