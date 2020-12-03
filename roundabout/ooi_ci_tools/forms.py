047e1a65453e688bb85c51d266cfa4b11abd1678
Mario Carloni <mcarloni4@gmail.com>
2020-12-03 14:06:24 -0500

    initial manufacturer serial number validation for calibration imports

--- roundabout/ooi_ci_tools/forms.py
+++ roundabout/ooi_ci_tools/forms.py
@@ -302,9 +302,31 @@ def validate_cal_files(csv_files,ext_files):
                 _('File: %(filename)s, %(value)s: More than one existing Deployment associated with File Deployment Date'),
                 params={'value': cal_date_string, 'filename': cal_csv.name},
             )
+        try:
+            serial_label_qs = inventory_item.fieldvalues.filter(field__field_name__iexact='Manufacturer Serial Number',is_current=True)
+            if serial_label_qs.exists():
+                inv_manufacturer_serial = serial_label_qs[0].field_value
+            else:
+                inv_manufacturer_serial = ''
+            if 'OPTAA' in inventory_item.part.name:
+                inv_manufacturer_serial = 'ACS-'+inv_manufacturer_serial
+        except:
+            raise ValidationError(
+                _('File: %(filename)s, %(value)s: Cannot find Manufacturer Serial Number for Inventory Item'),
+                params={'value': inventory_item, 'filename': cal_csv.name},
+            )
         for idx, row in enumerate(reader):
             row_data = row.items()
             for key, value in row_data:
+                if key == 'serial':
+                    csv_manufacturer_serial = value.strip()
+                    try:
+                        assert csv_manufacturer_serial == inv_manufacturer_serial
+                    except:
+                        raise ValidationError(
+                            _('File: %(filename)s, Serial Number: %(value)s, Row %(row)s: Manufacturer Serial Numbers differ between CSV and Inventory Item'),
+                            params={'value': csv_manufacturer_serial, 'row': idx, 'filename': cal_csv.name},
+                        )
                 if key == 'name':
                     calibration_name = value.strip()
                     try:
