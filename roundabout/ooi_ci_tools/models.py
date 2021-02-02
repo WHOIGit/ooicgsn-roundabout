53c34b0a680eba8119f8fc8d2463addef12e1585
Mario Carloni <mcarloni4@gmail.com>
2021-02-02 12:01:29 -0500

    cleanup of mptt comment model, modified node type and ids of add, edit, and delete button logic

--- roundabout/ooi_ci_tools/models.py
+++ roundabout/ooi_ci_tools/models.py
@@ -18,18 +18,3 @@ class Comment(models.Model):
     action = models.ForeignKey(Action, related_name='comments', on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, related_name='comments', on_delete=models.SET_NULL, null=True)
     detail = models.TextField(blank=True)
-
-
-# class CommentNode(MPTTModel):
-#     class MPTTMeta:
-#         order_insertion_by = ['-created_at']
-#     def __str__(self):
-#         return self.detail
-#     def get_object_type(self):
-#         return 'comment_node'
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     parent = TreeForeignKey('self', related_name='children', on_delete=models.CASCADE, null=True, blank=True, db_index=True)
-#     action = models.ForeignKey(Action, related_name='comment_nodes', on_delete=models.CASCADE, null=True)
-#     user = models.ForeignKey(User, related_name='comment_nodes', on_delete=models.SET_NULL, null=True)
-#     detail = models.TextField(blank=True)
