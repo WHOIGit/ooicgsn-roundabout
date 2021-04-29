7738fdcb96e7c24f77b47ca5947cc80a3e84ec51
Sidney Batchelder <sbatchelder@whoi.edu>
2021-04-29 16:46:01 -0400

    multi-table pages now have functional download buttons, where each button downloads its own table. Affects User search and CCC value-change search

--- roundabout/search/mixins.py
+++ roundabout/search/mixins.py
@@ -22,6 +22,7 @@
 from django.http import StreamingHttpResponse
 from django_tables2.export import TableExport
 from django_tables2.export.views import ExportMixin
+from django_tables2.templatetags.django_tables2 import register,QuerystringNode
 from tablib import Dataset
 
 class TableExportStream(TableExport):
@@ -61,3 +62,39 @@ class ExportStreamMixin(ExportMixin):
 # https://github.com/django-import-export/django-import-export/issues/206#issuecomment-567935233
 # https://docs.djangoproject.com/en/2.2/ref/request-response/#django.http.StreamingHttpResponse
 # https://stackoverflow.com/a/49820427
+
+class MultiExportMixin(ExportMixin):
+    export_trigger_param_type = "_exportfmt"
+    export_trigger_param_table = "_exportidx"
+    def create_export(self, table_index, export_format):
+        table = self.get_tables()[table_index]
+        exporter = self.export_class(
+            export_format=export_format,
+            table=table,
+            exclude_columns=self.exclude_columns,
+        )
+
+        return exporter.response(filename=self.get_export_filename(export_format))
+
+    def render_to_response(self, context, **kwargs):
+        export_format = self.request.GET.get(self.export_trigger_param_type, None)
+        table_index = self.request.GET.get(self.export_trigger_param_table, None)
+        if self.export_class.is_valid_format(export_format):
+            table_index = int(table_index)
+            return self.create_export(table_index, export_format)
+
+        return super().render_to_response(context, **kwargs)
+
+
+@register.simple_tag(takes_context=True)
+def multiexport_url(context, table_idx, export_format, export_trigger_param_type=None, export_trigger_param_table=None):
+    if export_trigger_param_type is None and "view" in context:
+        export_trigger_param_type = getattr(context["view"], "export_trigger_param_type", None)
+    if export_trigger_param_table is None and "view" in context:
+        export_trigger_param_table = getattr(context["view"], "export_trigger_param_table", None)
+    export_trigger_param_type = export_trigger_param_type or "_exportfmt"
+    export_trigger_param_table = export_trigger_param_table or "_exportidx"
+
+    return QuerystringNode(updates={export_trigger_param_type:export_format,
+                                    export_trigger_param_table:table_idx},
+                           removals=[]).render(context)
