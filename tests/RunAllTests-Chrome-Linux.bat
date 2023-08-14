737d7b0185994d20f291d26fa78b974ac63f0789
Joanne Koch <jkoch@whoi.edu>
2023-08-14 14:01:17 -0400

    Update Chromedriver location

--- tests/RunAllTests-Chrome-Linux.bat
+++ tests/RunAllTests-Chrome-Linux.bat
@@ -3,7 +3,7 @@
 set -e
 
 google-chrome --version
-chromedriver --version
+/usr/local/bin/chromedriver-linux64/chromedriver.exe --version
 node -v
 
 # Runs all Roundabout Selenium Webdriver automated tests in linux Docker container. Takes about 14 minutes to run.
